76beab27a6423ed0f8b7bbf3db649cea08cc6221
saidane80 <115323246+saidane80@users.noreply.github.com>
2022-11-11 15:14:26 +0100

    Create Election.c

---
+++ Election.c
@@ -0,0 +1,82 @@
+# crew-x-
+#include "election.h"
+
+int ajouter(char * filename, Election e )
+{
+    FILE * f=fopen(filename, "a");
+    if(f!=NULL)
+    {
+        fprintf(f,"%d %d %d %d %d %d %s\n",e.identifiant,e.nombre_habitant,e.nombre_conseillers,e.D.jour,e.D.mois,e.D.annee,e.municipalite);
+        fclose(f);
+        return 1;
+    }
+    else return 0;
+}
+int modifier( char * filename, int id, Election nouv )
+{
+    int tr=0;
+    Election e;
+    FILE * f=fopen(filename, "r");
+    FILE * f2=fopen("nouv.txt", "w");
+    if(f!=NULL && f2!=NULL)
+    {
+        while(fscanf(f,"%d %d %d %d %d %d %s\n",&e.identifiant,&e.nombre_habitant,&e.nombre_conseillers,&e.D.jour,&e.D.mois,&e.D.annee,e.municipalite)!=EOF)
+        {
+            if(e.identifiant== id)
+            {
+                fprintf(f2,"%d %d %d %d %d %d %s\n",nouv.identifiant,nouv.nombre_habitant,nouv.nombre_conseillers,nouv.D.jour,nouv.D.mois,nouv.D.annee,nouv.municipalite);
+                tr=1;
+            }
+            else
+                fprintf(f2,"%d %d %d %d %d %d %s\n",e.identifiant,e.nombre_habitant,e.nombre_conseillers,e.D.jour,e.D.mois,e.D.annee,e.municipalite);
+
+        }
+    }
+    fclose(f);
+    fclose(f2);
+    remove(filename);
+    rename("nouv.txt", filename);
+    return tr;
+
+}
+int supprimer(char * filename, int id)
+{
+    int tr=0;
+    Election e;
+    FILE * f=fopen(filename, "r");
+    FILE * f2=fopen("nouv.txt", "w");
+    if(f!=NULL && f2!=NULL)
+    {
+        while(fscanf(f,"%d %d %d %d %d %d %s\n",&e.identifiant,&e.nombre_habitant,&e.nombre_conseillers,&e.D.jour,&e.D.mois,&e.D.annee,e.municipalite)!=EOF)
+        {
+            if(e.identifiant== id)
+                tr=1;
+            else
+                fprintf(f,"%d %d %d %d %d %d %s\n",e.identifiant,e.nombre_habitant,e.nombre_conseillers,e.D.jour,e.D.mois,e.D.annee,e.municipalite);
+        }
+    }
+    fclose(f);
+    fclose(f2);
+    remove(filename);
+    rename("nouv.txt", filename);
+    return tr;
+}
+Election chercher(char * filename, int id)
+{
+    Election e;
+    int tr=0;
+    FILE * f=fopen(filename, "r");
+    if(f!=NULL)
+    {
+        while(tr==0 && fscanf(f,"%d %d %d %d %d %d %s\n",&e.identifiant,&e.nombre_habitant,&e.nombre_conseillers,&e.D.jour,&e.D.mois,&e.D.annee,e.municipalite)!=EOF)
+        {
+            if(e.identifiant== id)
+                tr=1;
+        }
+    }
+    fclose(f);
+    if(tr==0)
+        e.identifiant=-1;
+    return e;
+
+}
