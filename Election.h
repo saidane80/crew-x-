90ab38c2f003d6cb47b627d0ead02151f15be403
saidane80 <115323246+saidane80@users.noreply.github.com>
2022-11-16 12:33:05 +0100

    Update Election.h

--- Election.h
+++ Election.h
@@ -6,14 +6,16 @@ typedef struct date{
 int jour,mois,annee;
 }Date ;
 typedef struct election{
-int identifiant,nombre_habitant,nombre_conseillers;
+char identifiant[50];
+int nombre_habitant,nombre_conseillers;
 Date D;
 char municipalite[20];
 
 }Election;
 int ajouter(char *, Election );
-int modifier( char *, int, Election );
-int supprimer(char *, int );
-Election chercher(char *, int);
+int modifier( char *, char[], Election );
+int supprimer(char *, char[] );
+Election chercher(char *, char[]);
 
 #endif // ELECTION_H_INCLUDED
+
