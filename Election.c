4bd42f6897547fcc955cd222d20e4bf139f8fd27
saidane80 <115323246+saidane80@users.noreply.github.com>
2022-11-16 12:32:09 +0100

    Update Election.c

--- Election.c
+++ Election.c
@@ -1,18 +1,19 @@
 # crew-x-
 #include "election.h"
+#include <string.h>
 
 int ajouter(char * filename, Election e )
 {
     FILE * f=fopen(filename, "a");
     if(f!=NULL)
     {
-        fprintf(f,"%d %d %d %d %d %d %s\n",e.identifiant,e.nombre_habitant,e.nombre_conseillers,e.D.jour,e.D.mois,e.D.annee,e.municipalite);
+        fprintf(f,"%s %d %d %d %d %d %s\n",e.identifiant,e.nombre_habitant,e.nombre_conseillers,e.D.jour,e.D.mois,e.D.annee,e.municipalite);
         fclose(f);
         return 1;
     }
     else return 0;
 }
-int modifier( char * filename, int id, Election nouv )
+int modifier( char * filename, char id[], Election nouv )
 {
     int tr=0;
     Election e;
@@ -20,15 +21,15 @@ int modifier( char * filename, int id, Election nouv )
     FILE * f2=fopen("nouv.txt", "w");
     if(f!=NULL && f2!=NULL)
     {
-        while(fscanf(f,"%d %d %d %d %d %d %s\n",&e.identifiant,&e.nombre_habitant,&e.nombre_conseillers,&e.D.jour,&e.D.mois,&e.D.annee,e.municipalite)!=EOF)
+        while(fscanf(f,"%s %d %d %d %d %d %s\n",e.identifiant,&e.nombre_habitant,&e.nombre_conseillers,&e.D.jour,&e.D.mois,&e.D.annee,e.municipalite)!=EOF)
         {
-            if(e.identifiant== id)
+            if(strcmp(e.identifiant,id)== 0)
             {
-                fprintf(f2,"%d %d %d %d %d %d %s\n",nouv.identifiant,nouv.nombre_habitant,nouv.nombre_conseillers,nouv.D.jour,nouv.D.mois,nouv.D.annee,nouv.municipalite);
+                fprintf(f2,"%s %d %d %d %d %d %s\n",nouv.identifiant,nouv.nombre_habitant,nouv.nombre_conseillers,nouv.D.jour,nouv.D.mois,nouv.D.annee,nouv.municipalite);
                 tr=1;
             }
             else
-                fprintf(f2,"%d %d %d %d %d %d %s\n",e.identifiant,e.nombre_habitant,e.nombre_conseillers,e.D.jour,e.D.mois,e.D.annee,e.municipalite);
+                fprintf(f2,"%s %d %d %d %d %d %s\n",e.identifiant,e.nombre_habitant,e.nombre_conseillers,e.D.jour,e.D.mois,e.D.annee,e.municipalite);
 
         }
     }
@@ -39,7 +40,7 @@ int modifier( char * filename, int id, Election nouv )
     return tr;
 
 }
-int supprimer(char * filename, int id)
+int supprimer(char * filename, char id[])
 {
     int tr=0;
     Election e;
@@ -47,12 +48,12 @@ int supprimer(char * filename, int id)
     FILE * f2=fopen("nouv.txt", "w");
     if(f!=NULL && f2!=NULL)
     {
-        while(fscanf(f,"%d %d %d %d %d %d %s\n",&e.identifiant,&e.nombre_habitant,&e.nombre_conseillers,&e.D.jour,&e.D.mois,&e.D.annee,e.municipalite)!=EOF)
+        while(fscanf(f,"%s %d %d %d %d %d %s\n",e.identifiant,&e.nombre_habitant,&e.nombre_conseillers,&e.D.jour,&e.D.mois,&e.D.annee,e.municipalite)!=EOF)
         {
-            if(e.identifiant== id)
+            if(strcmp(e.identifiant,id)== 0)
                 tr=1;
             else
-                fprintf(f,"%d %d %d %d %d %d %s\n",e.identifiant,e.nombre_habitant,e.nombre_conseillers,e.D.jour,e.D.mois,e.D.annee,e.municipalite);
+                fprintf(f2,"%s %d %d %d %d %d %s\n",e.identifiant,e.nombre_habitant,e.nombre_conseillers,e.D.jour,e.D.mois,e.D.annee,e.municipalite);
         }
     }
     fclose(f);
@@ -61,22 +62,24 @@ int supprimer(char * filename, int id)
     rename("nouv.txt", filename);
     return tr;
 }
-Election chercher(char * filename, int id)
+Election chercher(char * filename, char id[])
 {
     Election e;
     int tr=0;
     FILE * f=fopen(filename, "r");
     if(f!=NULL)
     {
-        while(tr==0 && fscanf(f,"%d %d %d %d %d %d %s\n",&e.identifiant,&e.nombre_habitant,&e.nombre_conseillers,&e.D.jour,&e.D.mois,&e.D.annee,e.municipalite)!=EOF)
+        while(tr==0 && fscanf(f,"%s %d %d %d %d %d %s\n",e.identifiant,&e.nombre_habitant,&e.nombre_conseillers,&e.D.jour,&e.D.mois,&e.D.annee,e.municipalite)!=EOF)
         {
-            if(e.identifiant== id)
+            if(strcmp(e.identifiant,id)== 0)
                 tr=1;
         }
     }
     fclose(f);
     if(tr==0)
-        e.identifiant=-1;
+        strcpy(e.identifiant,"-1");
     return e;
 
 }
+
+
