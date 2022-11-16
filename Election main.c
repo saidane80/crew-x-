baa45f5098fdb42529ec1f09b807ec740724f91e
saidane80 <115323246+saidane80@users.noreply.github.com>
2022-11-16 12:33:43 +0100

    Update Election main.c

--- Election main.c
+++ Election main.c
@@ -6,23 +6,28 @@
 
 int main()
 {
-	Election e1={12699108,3000,10,02,05,2022,"tunis"},e2={12699105,4000,20,15,03,2022,"Ariana"},e3={14569870,5070,23,20,03,2022,"Manouba"};
+	Election e1={"abc123",3000,10,02,05,2022,"tunis"},e2={"efg456",4000,20,15,03,2022,"Ariana"},e3={"rtd789",5070,23,20,03,2022,"Manouba"},e4;
 	int x=ajouter("election.txt",e1 );
-	
+
 	if(x==1)
         printf("\najout de election avec succés");
       else printf("\nechec ajout");
-	x=modifier("election.txt",12699108,e2);
+         x=ajouter("election.txt",e3 );
+
+	if(x==1)
+        printf("\najout de election avec succés");
+      else printf("\nechec ajout");
+	x=modifier("election.txt","abc123",e2);
 
       if(x==1)
         printf("\nModification de election avec succés");
        else printf("\nechec Modification");
-	x=supprimer("election.txt",12699101 );
+	x=supprimer("election.txt","rtd789" );
     if(x==1)
         printf("\nSuppression de election avec succés");
     else printf("\nechec Suppression");
-    e3=chercher("election.txt",12699105 );
-    if(e3.identifiant==-1)
+    e4=chercher("election.txt","330" );
+    if(strcmp(e4.identifiant,"-1")==0)
         printf("\nintrouvable");
     return 0;
 }
